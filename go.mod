d99dbc189ff2163bac3afce60be135d59bb549b3
DoVietHoang1712 <hoang171220001@gmail.com>
2021-08-25 13:13:31 +0700

    Modify

--- go.mod
+++ go.mod
@@ -3,10 +3,19 @@ module github.com/DoVietHoang1712/golang-echo-tutorial
 go 1.16
 
 require (
+	github.com/0xAX/notificator v0.0.0-20210731104411-c42e3d4a43ee // indirect
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/codegangsta/envy v0.0.0-20141216192214-4b78388c8ce4 // indirect
+	github.com/codegangsta/gin v0.0.0-20171026143024-cafe2ce98974 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-playground/universal-translator v0.18.0 // indirect
+	github.com/go-playground/validator v9.31.0+incompatible // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible
+	github.com/ilyakaznacheev/cleanenv v1.2.5 // indirect
 	github.com/labstack/echo/v4 v4.5.0
+	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.13 // indirect
+	github.com/mattn/go-shellwords v1.0.12 // indirect
 	github.com/spf13/viper v1.8.1 // indirect
 	github.com/stretchr/testify v1.7.0 // indirect
 	golang.org/x/crypto v0.0.0-20210817164053-32db794688a5 // indirect
@@ -14,5 +23,7 @@ require (
 	golang.org/x/sys v0.0.0-20210820121016-41cdb8703e55 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20210723032227-1f47c861a9ac // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
 )
--- main.go
+++ main.go
@@ -7,19 +7,31 @@ import (
 
 	"github.com/DoVietHoang1712/golang-echo-tutorial/handler"
 	"github.com/DoVietHoang1712/golang-echo-tutorial/mdw"
+	"github.com/go-playground/validator"
 	"github.com/golang-jwt/jwt"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
 type User struct {
-	Name  string `json:"name" form:"name" query:"name"`
-	Email string `json:"email" form:"email" query:"email"`
+	Name  string `json:"name" form:"name" query:"name" validate:"required"`
+	Email string `json:"email" form:"email" query:"email" validate:"required,email"`
+}
+
+type CustomValidator struct {
+	validator *validator.Validate
+}
+
+func (cv *CustomValidator) Validate(i interface{}) error {
+	if err := cv.validator.Struct(i); err != nil {
+		return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+	}
+	return nil
 }
 
 func main() {
 	e := echo.New()
-
+	e.Validator = &CustomValidator{validator: validator.New()}
 	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
 		Format: "user_agent=${user_agent}, method=${method}, uri=${uri}, status=${status}\n",
 	}))
@@ -29,7 +41,16 @@ func main() {
 	e.GET("/", hello, isLogedIn)
 	e.GET("/product/:id", handler.GetProduct)
 	e.POST("/login", handler.Login, middleware.BasicAuth((mdw.BasicAuth)))
-
+	e.POST("product", func(context echo.Context) (err error) {
+		u := new(User)
+		if err = context.Bind(u); err != nil {
+			return echo.NewHTTPError(http.StatusBadRequest, err.Error())
+		}
+		if err = context.Validate(u); err != nil {
+			return err
+		}
+		return context.JSON(http.StatusOK, u)
+	})
 	e.POST("/test", func(context echo.Context) error {
 		u := new(User)
 		if err := context.Bind(u); err != nil {
