f78ad8b09683e09f18c8b16a266bfe4dbca0e548
DoVietHoang1712 <hoang171220001@gmail.com>
2021-09-01 09:22:21 +0700

    Modify

--- handler/product.go
+++ handler/product.go
@@ -7,6 +7,18 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+type Product struct {
+	ID primitive.ObjectID `json:"_id" bson:"_id"`
+	Name string `json:"product_name" bson:"product_name"`
+	Price int `json:"price" bson:"price"`
+	Currency string `json:"currency" bson:"currency"`
+	Quantity int `json:"quantity" bson:"quantity"`
+	Discount int `json:"discount,omitempty" bson:"discount,omitempty"`
+	Vendor string `json:"vendor" bson:"vendor"`
+	Accessories []string `json:"accessories,omitempty" bson:"accessories,omitempty"`
+	SkuID string `json:"sku_id" bson:"sku_id"`
+}
+
 func GetProduct(e echo.Context) error {
 	var products = []map[int]string{{1: "laptop"}, {2: "tv"}, {3: "mobile"}}
 	var product map[int]string
